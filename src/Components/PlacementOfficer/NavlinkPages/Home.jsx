d361d69f5178b3bfae21cb29e643401267e4123f
muhdfarseen <muhdfarseen@gmail.com>
2024-05-04 15:13:37 +0530

    feat: home routing

--- src/Components/PlacementOfficer/NavlinkPages/Home.jsx
+++ src/Components/PlacementOfficer/NavlinkPages/Home.jsx
@@ -21,20 +21,32 @@ import {
   IconSwitchHorizontal,
 } from "@tabler/icons-react";
 import classes from "./Home.module.css";
+import { useNavigate } from 'react-router-dom';
+
 
 const mockdata = [
-  { title: "Candidates", icon: IconUsers, color: "indigo" },
-  { title: "Placements", icon: IconBriefcase, color: "blue" },
-  { title: "Announcements", icon: IconCalendarStats, color: "green" },
-  { title: "Trainings", icon: IconChecklist, color: "teal" },
-  { title: "Settings", icon: IconSettings, color: "cyan" },
+  { title: "Candidates", icon: IconUsers, color: "indigo", linkto: "candidates" },
+  { title: "Placements", icon: IconBriefcase, color: "blue", linkto: "placements"  },
+  { title: "Announcements", icon: IconCalendarStats, color: "green", linkto: "announcements"  },
+  { title: "Trainings", icon: IconChecklist, color: "teal", linkto: "trainings"  },
+  { title: "Settings", icon: IconSettings, color: "cyan", linkto: "settings"  },
 ];
 
 export const Home = () => {
   const theme = useMantineTheme();
 
+  const Navigate = useNavigate();
+
+  const HandleNavigation = (linkto) => {
+    Navigate(`/dashboard/${linkto}`);
+  };
+
   const items = mockdata.map((item) => (
-    <UnstyledButton key={item.title} className={classes.item}>
+    <UnstyledButton
+      onClick={() => HandleNavigation(item.linkto)} 
+      key={item.title}
+      className={classes.item}
+    >
       <item.icon color={theme.colors[item.color][6]} size="2rem" />
       <Text size="xs" mt={7}>
         {item.title}
